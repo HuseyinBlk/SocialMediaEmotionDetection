e38a72e6c8afc2303cab5dcc211d23695afc6866
Umut ozkan <umuttozkannis@gmail.com>
2024-11-01 19:50:49 +0300

    add Comment repository

--- app/src/main/java/com/hope/socialmediaemotiondetection/model/user/comment/Comment.kt
+++ app/src/main/java/com/hope/socialmediaemotiondetection/model/user/comment/Comment.kt
@@ -3,6 +3,6 @@ package com.hope.socialmediaemotiondetection.model.user.comment
 data class Comment(
     val postId: String,
     val content: String,
-    val createdAt: String,
+    val createdAt: Any,
     val emotion: String
 )
---
+++ app/src/main/java/com/hope/socialmediaemotiondetection/repository/UserCommentsRepository.kt
@@ -0,0 +1,56 @@
+package com.hope.socialmediaemotiondetection.repository
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.hope.socialmediaemotiondetection.model.user.comment.Comment
+import kotlinx.coroutines.tasks.await
+
+class UserCommentsRepository(
+    private val auth: FirebaseAuth,
+    private val firestore: FirebaseFirestore
+) {
+
+    suspend fun addCommentToUser(postId: String, content: String, emotion: String) : Result<Boolean> {
+        val currentUser = auth.currentUser ?: return Result.failure(Exception("User not logged in"))
+        val commentId = firestore.collection("users").document(currentUser.uid)
+            .collection("comments").document().id // Yeni bir belge oluşturmak için ID al
+
+        val comment = Comment(
+            postId,
+            content,
+            FieldValue.serverTimestamp(),
+            emotion
+        )
+
+        return try {
+            firestore.collection("users").document(currentUser.uid)
+                .collection("comments").document(commentId).set(comment).await()
+            Result.success(true)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun getUserComments(userId: String): Result<Map<String, Comment>> {
+        return try {
+            val commentsSnapshot = firestore.collection("users").document(userId)
+                .collection("comments").get().await()
+
+            // Yorumları id ile birliktedöndür
+            val commentsMap = commentsSnapshot.documents.associate { doc ->
+                val comment = Comment(
+                    postId = doc.getString("postId") ?: "",
+                    content = doc.getString("content") ?: "",
+                    createdAt = doc.get("createdAt") ?: "",
+                    emotion = doc.getString("emotion") ?: ""
+                )
+                doc.id to comment
+            }
+
+            Result.success(commentsMap)
+        } catch (e: Exception) {
+            Result.failure(Exception("Failed to fetch user comments: ${e.message}"))
+        }
+    }
+}
