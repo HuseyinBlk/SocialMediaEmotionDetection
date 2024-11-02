4b740c6a065a1effd79c812d642b89bf83b821cb
Umut ozkan <umuttozkannis@gmail.com>
2024-11-02 12:25:03 +0300

    add dagger hilt module and agp upp

---
+++ app/src/main/java/com/hope/socialmediaemotiondetection/SocialMediaEmotionApp.kt
@@ -0,0 +1,8 @@
+package com.hope.socialmediaemotiondetection
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SocialMediaEmotionApp() : Application() {
+}
---
+++ app/src/main/java/com/hope/socialmediaemotiondetection/dependencyInjection/AppModule.kt
@@ -0,0 +1,4 @@
+package com.hope.socialmediaemotiondetection.dependencyInjection
+
+object AppModule {
+}
